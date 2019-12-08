0ad1561863c2f5706c77be98881d7de10917d558
kmatusz <kmatuszelanski@gmail.com>
2019-12-08 20:09:25 +0100

    testing my popularity vs. similar artists

--- 04_analysis.R
+++ 04_analysis.R
@@ -13,4 +13,23 @@ head(similar_artists, 7)
 head(tags, 7)
 # Rank- 1 is the most similar
 
+# Test my popularity vs similar artists ----
+similar_artists %>%
+  left_join(general_info, by = c("similar" = "name")) %>%
+  select(-listeners, -rank) %>%
+  group_by(artist) %>%
+  summarise(med_playcount = median(playcount, na.rm = T),
+            mean_playcount = mean(playcount, na.rm = T)) -> similar_agg
+
+
+general_info %>%
+  select(-listeners) %>%
+  left_join(similar_agg, by = c("name" = "artist")) -> both
+
+
+both %>%
+  mutate(my_others_ratio = playcount/mean_playcount) %>%
+  summary()
+
+# Artists recommended as similar have 20x bigger playcount
 
