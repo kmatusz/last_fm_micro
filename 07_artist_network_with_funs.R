8d9313a010a2eb2610ac0d274f1ead8890ce04ba
kmatusz <kmatuszelanski@gmail.com>
2019-12-18 23:45:44 +0100

    added simulation with parameter grid

--- 07_artist_network_with_funs.R
+++ 07_artist_network_with_funs.R
@@ -34,10 +34,19 @@ no_steps <- 50
 snowball_prob <- 0.07
 method <- "similar"
 
-a <- run_simulation(no_agents, no_steps, snowball_prob, similar_graph, similar_artists, method = method)
+results <- run_simulation(no_agents, no_steps, snowball_prob, similar_graph, similar_artists, method = method)
+
+a <- list(no_agents = 10000,
+          no_steps = 50,
+          snowball_prob = 0.07,
+          similar_graph, 
+          similar_artists,
+          method = "similar")
+do.call(run_simulation, a)
+results <- run_simulation(no_agents, no_steps, snowball_prob, similar_graph, similar_artists, method = method)
 
 
-similar_graph <- a$similar_graph
+similar_graph <- results$similar_graph
 
 params_title <- sprintf("no_agents: %s, no_steps: %s, snowball_prob: %s, method: %s",
                         no_agents,
@@ -67,13 +76,103 @@ tibble(
 # no_steps <- 50
 # snowball_prob <- 0.05
 # Good fit
+params_to_test <- list(
+list(no_agents = 10000,
+     no_steps = 50,
+     snowball_prob = 0.05,
+     similar_graph, 
+     similar_artists,
+     method = "similar"),
+list(no_agents = 10000,
+     no_steps = 50,
+     snowball_prob = 0.07,
+     similar_graph, 
+     similar_artists,
+     method = "similar"),
+list(no_agents = 10000,
+     no_steps = 50,
+     snowball_prob = 0.1,
+     similar_graph, 
+     similar_artists,
+     method = "similar"),
+list(no_agents = 10000,
+     no_steps = 50,
+     snowball_prob = 0.5,
+     similar_graph, 
+     similar_artists,
+     method = "similar"),
+list(no_agents = 10000,
+     no_steps = 50,
+     snowball_prob = 0.7,
+     similar_graph, 
+     similar_artists,
+     method = "similar"),
+list(no_agents = 10000,
+     no_steps = 50,
+     snowball_prob = 0.9,
+     similar_graph, 
+     similar_artists,
+     method = "similar"),
+
+# Random (unif)
+list(no_agents = 10000,
+     no_steps = 50,
+     snowball_prob = 0.05,
+     similar_graph, 
+     similar_artists,
+     method = "unif"),
+list(no_agents = 10000,
+     no_steps = 50,
+     snowball_prob = 0.07,
+     similar_graph, 
+     similar_artists,
+     method = "unif"),
+list(no_agents = 10000,
+     no_steps = 50,
+     snowball_prob = 0.1,
+     similar_graph, 
+     similar_artists,
+     method = "unif"),
+list(no_agents = 10000,
+     no_steps = 50,
+     snowball_prob = 0.5,
+     similar_graph, 
+     similar_artists,
+     method = "unif"),
+list(no_agents = 10000,
+     no_steps = 50,
+     snowball_prob = 0.7,
+     similar_graph, 
+     similar_artists,
+     method = "unif"),
+list(no_agents = 10000,
+     no_steps = 50,
+     snowball_prob = 0.9,
+     similar_graph, 
+     similar_artists,
+     method = "unif")
+)
+
+results_list <- vector("list", length(params_to_test))
+
+for (i in 1:length(params_to_test)){
+  print(sprintf("Result %s", i))
+  results_list[[i]] <- do.call(run_simulation, params_to_test[[i]])[[1]]
+}
+
+save(params_to_test, results_list, file = "data/simulation_results.Rdata")
 
 
+similar_graph <- results_list[[12]]
+tibble(
+  playcount = vertex_attr(similar_graph)$playcount,
+  simulation_playcount = vertex_attr(similar_graph)$simulation_playcount
+) %>%
+  mutate(simulation_playcount = simulation_playcount * max(playcount) /
+           max(simulation_playcount)) %>%
+  pivot_longer(1:2) %>%
+  ggplot(aes(value, color = name, fill = name)) +
+  stat_ecdf() #+
+  # xlim(c(0, 1.0e+06)) +
+  # labs(title = params_title)
 
-
-
-# 50 steps enough to converge
-# For just 0.05 prob of choosing popular we get results close to original data (without inputing this information)
-
-
-# After 10 steps
